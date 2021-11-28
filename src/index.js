43c1d2c0dc14b40b4d86458e700f877ded1499f9
Bostaji Thomas <thomas.bostaji@student.vinci.be>
2021-11-28 23:52:43 +0100

    update : index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+/*
 // Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
 
 // This is the entry point to your app : add all relevant import and custom code
+*/
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./stylesheets/main.css"
+import NavBar from "./Components//Navbar";
+import { Router } from "./Components/Router";
+
+
+NavBar();
+
+Router();
