042541a54d35cd30f4abf895438ed892177b4696
32494 <amine.nemmaoui@student.vinci.be>
2021-12-13 09:38:45 +0100

    debut des socket

--- src/Components/Pages/WaitingGamePage.js
+++ src/Components/Pages/WaitingGamePage.js
@@ -15,7 +15,10 @@ waitingPage = `
 <div class="row" id="homePage">
   <div class="col"></div>
   <div class="col text-center">
-  <div id="waiting"></div>
+  <div id="waiting">
+    <div id="players"></div>
+    <div id="bouton"></div> 
+  </div>
 </div>
 <div class="col"></div>
 </div>
@@ -24,35 +27,37 @@ waitingPage = `
 function WaitingGamePage() {
   // reset #page div
   //addPlayer();
+  
 
   const pageDiv = document.querySelector("#page");
-
   pageDiv.innerHTML = waitingPage;
+  getPlayer();
 }
 
-if (getSessionObject("room") !== undefined) {
-  socket.emit("joinRoom", {
-    id: getSessionObject("room").id,
-    username: getSessionObject("user").username,
-  });
+function getPlayer() {
+  if (getSessionObject("room") !== undefined) {
+    socket.emit("joinRoom", {
+      id: getSessionObject("room").id,
+      username: getSessionObject("user").username,
+    });
 
-  socket.emit("playerList", getSessionObject("room").id);
-  socket.on("list players", (players) => {
-    players.forEach((e) => {
-      console.log(e);
-      document.getElementById(
-        "waiting"
-      ).innerHTML += `<li class="list-group-item d-flex justify-content-between">
+    //socket.emit("playerList", getSessionObject("room").id);
+    socket.on("playersList", ({ rooms }) => {
+      console.log(rooms);
+      rooms.forEach((e) => {
+        console.log(e);
+        document.getElementById(
+          "players"
+        ).innerHTML += `<li class="list-group-item d-flex justify-content-between">
                       <p class="p-0 m-0 flex-grow-1 fw-bold" id="room-dispo">Joueur - ${e}</p>
                     </li>`;
-    });
-    if (players.length > 1) {
-      document.getElementById(
-        "waiting"
-      ).innerHTML += `<p></p>
+      });
+      if (rooms.length > 1) {
+        document.getElementById("bouton").innerHTML = `<p></p>
                       <input type="submit" class="btn btn-sm btn-success join-room" id="inputJoin" value="Lancer">`;
-    }
-  });
+      }
+    });
+  }
 }
 
 export default WaitingGamePage;
